d937b361b296f09660bb635b8017c1eef27bdcc4
MDPuneethReddy <puneethmotukuru@gmail.com>
2021-12-04 16:41:42 +0530

    Remove category error

--- pages/blog/category.tsx
+++ pages/blog/category.tsx
@@ -1,27 +1,28 @@
  import type { NextPage } from 'next'
-// import Head from 'next/head'
-// import React from 'react'
+import Head from 'next/head'
+import React from 'react'
 // import Post from '../../components/Post'
 // import { sortByDate } from '../../utils'
 // import { getPostsByCategory } from '../api/blogposts/utils'
-// interface Iprops{
-//   posts:Array<any>,
-//   category:string
-// }
-//  const Category: NextPage<Iprops> = (props:Iprops) => {
-//   return (
-//     <div>
-//       <Head>
-//         <title>{props.category}</title>
-//       </Head>
-//       <div className='posts'>
-//         {props.posts.map((post:any, index:number) => (
-//           <Post key={index} post={post} />
-//         ))}
-//       </div>
-//     </div>
-//   )
-// }
+interface Iprops{
+  posts:Array<any>,
+  category:string
+}
+ const Category: NextPage<Iprops> = (props:Iprops) => {
+  return (
+    <div>
+      
+       {/* <Head>
+       <title>{props.category}</title>
+      </Head>
+      <div className='posts'>
+        {props.posts.map((post:any, index:number) => (
+          <Post key={index} post={post} />
+        ))}
+      </div> */}
+    </div>
+  )
+}
 // export const getServerSideProps=async(context:any)=> {
 //   const {category} = context.query
 //   const {posts}=await getPostsByCategory(category)
@@ -32,4 +33,4 @@
 //     },
 //   }
 // }
-// export default Category
+export default Category
